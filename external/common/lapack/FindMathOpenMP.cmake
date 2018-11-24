8196ed0a457f9a197c105ca162d8f7f7aea4079d
Raimondas Galvelis <raimondas.galvelis@gmail.com>
2018-11-24 02:13:38 +0100

    Debug 5

--- external/common/lapack/FindMathOpenMP.cmake
+++ external/common/lapack/FindMathOpenMP.cmake
@@ -102,7 +102,7 @@ endif()
 if (ENABLE_OPENMP)
     # *not* REQUIRED b/c some compilers don't support OpenMP and -DENABLE_OPENMP isn't a build-or-die-trying
     find_package(TargetOpenMP COMPONENTS ${TargetOpenMP_FIND_COMPONENTS})
-    if (TARGET OpenMP::OpenMP)
+    if (TargetOpenMP_FOUND)
         target_link_libraries(tgt::MathOpenMP INTERFACE OpenMP::OpenMP)
     else()
         message(WARNING "OpenMP configuration failed! The code will be built without OpenMP.")
